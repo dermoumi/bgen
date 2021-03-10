93b971e9e093228e43f3e1aed80c21261c8888f1
Dermoumi S <hi@sdrm.me>
2021-03-10 07:53:25 +0100

    fix: arguments of the run subcommand mismatched

--- src/run.sh
+++ src/run.sh
@@ -3,6 +3,11 @@
 bgen:import build
 
 command_run() {
+    barg.arg input \
+        --short=i \
+        --long=input \
+        --value=FILE \
+        --desc "File or directory to run, if any"
     barg.arg debug \
         --short=d \
         --long=debug \
@@ -12,6 +17,7 @@ command_run() {
         --value=ARGUMENT \
         --desc="Arguments to pass to the project."
 
+    local input=
     local debug=
     local args=()
 
@@ -42,21 +48,49 @@ command_run() {
     local is_library=
     read_project_meta
 
+    if [[ "$input" == "" ]]; then
+        input=$entrypoint_file
+    fi
+
     if ((is_library)); then
         butl.die "You cannot run library projects."
     elif ((debug)); then
-        debug_project "${args[@]}"
+        debug_project "$input" "${args[@]}"
     else
-        run_project "${args[@]}"
+        run_project "$input" "${args[@]}"
     fi
 }
 
 run_project() {
+    local input=$1
+
+    local tmp_file
+    tmp_file=$(mktemp)
+    build_file "$input" >"$tmp_file"
+
     export __BGEN_PIPE_SOURCE__="${__base__:-$0} run"
-    bash -c "$(build_file "$entrypoint_file")" "${__base__:-$0} run" "$@"
+
+    local err=0
+    bash "$tmp_file" "$@" || err=$?
+
+    rm -f "$tmp_file"
+
+    return "$err"
 }
 
 debug_project() {
-    export __BGEN_PIPE_SOURCE__="${__base__:-$0} debug"
-    bash -x -c "$(build_file "$entrypoint_file")" "${__base__:-$0} run" "$@"
+    local input=$1
+
+    local tmp_file
+    tmp_file=$(mktemp)
+    build_file "$input" >"$tmp_file"
+
+    export __BGEN_PIPE_SOURCE__="${__base__:-$0} run"
+
+    local err=0
+    bash -x "$tmp_file" "$@" || err=$?
+
+    rm -f "$tmp_file"
+
+    return "$err"
 }
